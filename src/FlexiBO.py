bcfb8d9d48f108802239554c20093f031dc8be25
iqbal128855 <mdshahriariqbal@knights.ucf.edu>
2019-08-24 02:44:45 -0400

    Updated eval status and measurement across objective

--- src/FlexiBO.py
+++ src/FlexiBO.py
@@ -1,8 +1,8 @@
-from __future__ import division
 import math
 import Config
 import numpy as np
 from Utils import Utils
+from Sample import Sample
 from SurrogateModel import SurrogateModel
 from sklearn.ensemble import RandomForestRegressor
 
@@ -19,6 +19,7 @@ class FlexiBO(object):
                 data):
         print ("Initializing FlexiBO class")
         self.utils=Utils()
+        self.sample=Sample()
         self.df=data
         (self.E, self.O, self.measurement)=self.utils.create_design_space(Config.bounds)
         self.NUM_ITER=5
@@ -27,14 +28,11 @@ class FlexiBO(object):
         self.O2_IND=0
         self.m1="inference_time"
         self.m2="temperature"
-        self.O1_COST=1
-        self.O2_COST=self.O1_COST*8
         self.SM=SurrogateModel()
         (self.X, self.Y1, self.Y2)=self.prepare_training_data()
         self.fit_rf()
         self.perform_bo_loop()
         
-    
     def fit_rf(self):
         """This function is used to predict 
         """
@@ -47,12 +45,12 @@ class FlexiBO(object):
         """This function is used to prepare training data
         """
         X=self.df[['core0_status',
-                         'core1_status', 
-                         'core2_status', 
-                         'core3_status',
-                         'core_freq',
-                         'gpu_freq',
-                         'emc_freq']].values
+                   'core1_status', 
+                   'core2_status', 
+                   'core3_status',
+                   'core_freq',
+                   'gpu_freq',
+                   'emc_freq']].values
         
         Y1=self.df[self.m1].values
         Y2=self.df[self.m2].values
@@ -67,129 +65,29 @@ class FlexiBO(object):
         """This function is used to initialize data
         """
         import random
-        index=random.sample(range(0,len(self.X)-1),760)
+        index=random.sample(range(0,len(self.X)-1),10)
         X=[self.X[i] for i in index]
         Y1=[self.Y1[i] for i in index]
         Y2=[self.Y2[i] for i in index]
         return (X,
                 Y1,
-                Y2)
-    
-    def determine_next_sample(self,
-                             pess_pareto,
-                             opt_pareto,
-                             pess_indices_map,
-                             opt_indices_map,
-                             pess_pareto_volume,
-                             opt_pareto_volume):
-        """This function is used to determine next sample
-        """
-        if pess_indices_map==opt_indices_map:
-            indices_map=pess_indices_map
-        pess_ind=[indices_map[i] for i in xrange(len(pess_pareto))]
-        opt_ind=[indices_map[i] for i in xrange(len(opt_pareto))]
-        
-        pess_status=[[{"pess":True,"opt":True}] if i in opt_ind else [{"pess":True,"opt":False}] for i in pess_ind]
-        opt_status=[[{"pess":True,"opt":True}] if i in pess_ind else [{"pess":False,"opt":True}] for i in opt_ind]
-        
-        # compute dv/c for each point in pessimistic pareto front
-        dv_per_cost_pess=[{"o1":0,"o2":0} for i in pess_ind]
-        for i in xrange(len(pess_pareto)):                   
-             for j in xrange(self.NUM_OBJ):
-                         
-                 if pess_status[i][0]["pess"] is True:
-                     # Update pessimistic pareto front 
-                     cur_pess=pess_pareto[:]
-                     # replace pess with avg value across O1
-                     cur_pess[i][j]=self.REGION[pess_ind[i]]["avg"][j]
-                     cur_pess_pareto=self.utils.construct_pessimistic_pareto_front(pess_ind,
-                                                                              cur_pess,
-                                                                              "UPDATE")
-                     cur_pess_volume=self.utils.compute_pareto_volume(cur_pess_pareto)
-                 
-                 if pess_status[i][0]["opt"] is True:
-                     # Update optimistic pareto front 
-                     cur_opt=opt_pareto[:]
-                     opt_i=opt_ind.index(pess_ind[i])
-                     # replace opt with avg value across O1
-                     cur_opt[opt_i][j]=self.REGION[opt_ind[i]]["avg"][j]
-                     cur_opt_pareto=self.utils.construct_optimistic_pareto_front(opt_ind,
-                                                                                 cur_opt,
-                                                                                 "UPDATE")
-                     cur_opt_volume=self.utils.compute_pareto_volume(cur_opt_pareto)
-                 
-                 if pess_status[i][0]["opt"] is False:
-                     # No update
-                     cur_opt_volume=opt_pareto_volume
-                 
-                 dv=(opt_pareto_volume-pess_pareto_volume)-(cur_opt_volume-cur_pess_volume)
-                 if j==self.O1_IND:
-                     dv_per_cost_pess[i]["o1"]=dv/self.O1_COST
-                 if j==self.O2_IND:
-                     dv_per_cost_pess[i]["o2"]=dv/self.O2_COST
-        
-        # compute dv/c for each point in pessimistic pareto front
-        dv_per_cost_opt=[{"o1":0,"o2":0} for i in opt_ind]
-        for i in xrange(len(opt_pareto)):                   
-             for j in xrange(self.NUM_OBJ):
-                         
-                 if (opt_status[i][0]["opt"] is True and 
-                     opt_status[i][0]["opt"] is False):
-                     
-                     cur_opt=opt_pareto[:]
-                     # replace pess with avg value across O1
-                     cur_opt[i][j]=self.REGION[opt_ind[i]]["avg"][j]
-                     cur_opt_pareto=self.utils.construct_optimistic_pareto_front(opt_ind,
-                                                                              cur_opt,
-                                                                              "UPDATE")
-                     cur_opt_volume=self.utils.compute_pareto_volume(cur_opt_pareto)
-                     cur_pess_volume=pess_pareto_volume
-                 
-                 dv=(opt_pareto_volume-pess_pareto_volume)-(cur_opt_volume-cur_pess_volume)
-                 if j==self.O1_IND:
-                     dv_per_cost_pess[i]["o1"]=dv/self.O1_COST
-                 if j==self.O2_IND:
-                     dv_per_cost_pess[i]["o2"]=dv/self.O2_COST         
-        max_dv_per_cost=0
-        objective="o1"      
-        for i in xrange(len(dv_per_cost_pess)):
-            if abs(dv_per_cost_pess[i]["o1"])>=max_dv_per_cost:
-                max_dv_per_cost_ind=i
-                max_dv_per_cost=abs(dv_per_cost_pess[i]["o1"])
-                objective="o1"
-            if abs(dv_per_cost_pess[i]["o2"])>=max_dv_per_cost:
-                max_dv_per_cost_ind=i
-                max_dv_per_cost=abs(dv_per_cost_pess[i]["o2"])
-                objective="o2"
-        cur_dv_per_cost_ind=indices_map[max_dv_per_cost_ind]
-        for i in xrange(len(dv_per_cost_opt)):
-            if abs(dv_per_cost_opt[i]["o1"])>=max_dv_per_cost:
-                max_dv_per_cost_ind=i
-                max_dv_per_cost=abs(dv_per_cost_pess[i]["o1"])
-                objective="o1"
-            if abs(dv_per_cost_opt[i]["o2"])>=max_dv_per_cost:
-                max_dv_per_cost_ind=i
-                max_dv_per_cost=abs(dv_per_cost_pess[i]["o2"])
-                objective="o2"
-        cur_dv_per_cost_ind=indices_map[max_dv_per_cost_ind]
-        
-        next_sample=self.E[cur_dv_per_cost_ind]
-        
-        return (next_sample, objective)
-        
+                Y2,
+                index)
                            
     def perform_bo_loop(self):
         """This function is used to perform bayesian optimization loop
-        U: Undecided Set
-        S: Evaluated Set
+        U: Design Space
         REGION: Uncertainty Region for each configuration in design space
         """
         # Initialization
         BETA=1.0
-        P=list()
-        U=list()
-        S=list()
-        (init_X, init_Y1, init_Y2)=self.initialize()
+        (init_X, init_Y1, init_Y2, init_measured_indices)=self.initialize()
+        
+        for i in xrange(len(init_measured_indices)):
+            self.O[i]["o1"]=True
+            self.measurement[init_measured_indices[i]]["o1"]=init_Y1[i][0]
+            self.O[i]["o2"]=True
+            self.measurement[init_measured_indices[i]]["o2"]=init_Y2[i][0]    
         (init_X, init_Y1, init_Y2)=(
                                     np.array(init_X), 
                                     np.array(init_Y1), 
@@ -199,6 +97,7 @@ class FlexiBO(object):
         U=np.array(self.E[:])
         init_X1=init_X[:]
         init_X2=init_X[:]
+     
         # bo loop
         for iteration in xrange(self.NUM_ITER):
             print "---------------------------------------Iteration: ",iteration
@@ -260,27 +159,30 @@ class FlexiBO(object):
             # Determine volume of the pareto front
             volume_of_pareto_front=opt_pareto_volume-pess_pareto_volume
             # Determine next configuration and objective
-            (next_sample, objective)=self.determine_next_sample(pess_pareto,
-                                                   opt_pareto,
-                                                   pess_indices_map,
-                                                   opt_indices_map,
-                                                   pess_pareto_volume,
-                                                   opt_pareto_volume)
+            (next_sample_index, next_sample, objective)=self.sample.determine_next_sample(pess_pareto,
+                                                                                          opt_pareto,
+                                                                                          pess_indices_map,
+                                                                                          opt_indices_map,
+                                                                                          pess_pareto_volume,
+                                                                                          opt_pareto_volume,
+                                                                                          REGION,
+                                                                                          self.E)
             
             # Perform measurement on next sample on the objective returned
             # Update init_X and init_Y
             if objective=="o1":
                 cur_X1=np.array(next_sample)
                 cur_Y1=np.array(self.rf1.predict([cur_X1]))
+                self.O[next_sample_index]["o1"]=True
+                self.measurement[next_sample_index]["o1"]=cur_Y1[0]
                 np.vstack((init_X1,cur_X1))
                 np.vstack((init_Y1,cur_Y1))
             if objective=="o2":
                 cur_X2=np.array(next_sample)
                 cur_Y2=np.array(self.rf2.predict([cur_X2]))
+                self.O[next_sample_index]["o2"]=True
+                self.measurement[next_sample_index]["o2"]=cur_Y2[0]
                 np.vstack((init_X2,np.array(next_sample)))
                 np.vstack((init_Y2,cur_Y2))
             
-            
-          
-        
-       
+                   
